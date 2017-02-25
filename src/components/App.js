3999d7fde919c16b5ab4891ca4870586c417b64b
PakkuDon <pakku.pd@gmail.com>
2017-02-26 10:27:37 +1100

    Add topic grid to landing page

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,34 @@ import React, { Component } from 'react';
 import '../stylesheets/App.css';
 import Header from './Header';
 import Footer from './Footer';
+import topics from '../data/topics';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      availableTopics: {
+        isLoading: true,
+        topics: []
+      }
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      availableTopics: {
+        isLoading: false,
+        topics: topics
+      }
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <Header />
         <div>
-          {this.props.children}
+          {React.cloneElement(this.props.children, this.state)}
         </div>
         <Footer />
       </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import TopicList from './TopicList';
 
 export default class Home extends React.Component {
   render() {
     return (
-      <div className="video-wrapper">
-{/*        <h1>Home</h1>*/}
-        <iframe width="854" height="480" src="https://www.youtube.com/embed/oD-UoXUFnvk" frameborder="0" allowfullscreen></iframe>
+      <div>
+        <div className="video-wrapper">
+  {/*        <h1>Home</h1>*/}
+          <iframe width="854" height="480" src="https://www.youtube.com/embed/oD-UoXUFnvk" frameborder="0" allowfullscreen></iframe>
+        </div>
+        <TopicList topics={this.props.availableTopics.topics} />
       </div>
     );
   }
