6dd60ce6dd57e76b95d86b50a1ec43f98f2f066c
Lina <linachan.nz@gmail.com>
2017-02-25 20:29:49 +1100

    add video and footer

--- src/components/EmergencyButton.js
+++ src/components/EmergencyButton.js
@@ -5,7 +5,7 @@ export default class EmergencyButton extends React.Component {
   render() {
     return (
       <div className='emergency-button flex-1'>
-        <button className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">
+        <button className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect btn-large">
           I need help now
           <Link to='/panic'></Link>
         </button>
