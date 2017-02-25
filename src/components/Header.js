fd05b019f52f9b7d6c30ac59ac3e4cb2c162845a
Lina <linachan.nz@gmail.com>
2017-02-25 23:14:14 +1100

    add login button

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,8 +9,7 @@ export default class Header extends React.Component {
     return (
       <header className="App-header container flex-align-center">
         <h1 className="flex-3 heading-gold align-left"><small className="heading-small">The</small> Korin Gamadji <strong className="heading-orange">Institute</strong></h1>
-        <EmergencyButton />
-{/*        <Navbar />*/}
+        <Navbar />
       </header>
     );
   }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router';
+import EmergencyButton from './EmergencyButton';
 
 export default class Navbar extends React.Component {
   render() {
     return (
       <nav>
-        <ul>
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to='/topics'>Topics</Link></li>
+        <ul className="container flex-align-center nav-menu">
+          <li>
+            <button className="mdl-button mdl-button--fab mdl-js-button mdl-button--raised mdl-js-ripple-effect btn-login">
+              <Link to='/login'></Link>
+              <i className="material-icons">account_circle</i>
+            </button>
+          </li>
+          <li>
+            <EmergencyButton />
+          </li>
         </ul>
       </nav>
     );
