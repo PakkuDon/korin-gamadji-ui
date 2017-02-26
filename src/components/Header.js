1dda272429e0928cb99baa3e23e1e2cb90108905
Tess Elation <fredisded@gmail.com>
2017-02-26 12:48:14 +1100

    change header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default class Header extends React.Component {
   render() {
     return (
       <header className="App-header container flex-align-center">
-        <h1 className="flex-3 heading-gold align-left"> Korin Gamadji <strong className="heading-orange">Konnect</strong></h1>
+        <h1 className="flex-3 heading-gold align-left"> KGI <strong className="heading-orange">Konnect</strong></h1>
         <Navbar />
       </header>
     );
