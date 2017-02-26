bc853ffee0c58bc7c5dd23fddfbc6be4db660b4b
Lina <linachan.nz@gmail.com>
2017-02-26 11:51:36 +1100

    style topic list

--- src/components/TopicListItem.js
+++ src/components/TopicListItem.js
@@ -6,10 +6,10 @@ export default class TopicListItem extends React.Component {
     const { topic } = this.props;
     return (
       <div className='mdl-card topic-list-item mdl-cell mdl-cell--4-col'>
-        <div className='mdl-card__title'>
-          <h2><Link to={`/topics/${topic.id}`}>{topic.name}</Link></h2>
+        <div className='mdl-card__title topic-title'>
+          <h2 className="topic-header"><Link to={`/topics/${topic.id}`}>{topic.name}</Link></h2>
         </div>
-        <div className='mdl-card__media'>
+        <div className='mdl-card__media topic-body'>
           {/* TODO: Remove hard-coded dimensions*/}
           <img width="330px" height="200px" src={topic.image_url} alt={topic.name} />
         </div>
